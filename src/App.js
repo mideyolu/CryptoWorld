8bc6544813df1b870b1e59727593a1108daec9b9
mideyolu <olumideoluwuyi91@gmail.com>
2025-09-09 23:25:23 +0100

    Updated Component render flow, fixed refresh issue

--- src/App.js
+++ src/App.js
@@ -1,104 +1,101 @@
-import { Routes, Route } from "react-router-dom";
-import { Typography, Layout, Space } from "antd";
+import { Layout, Space, Typography } from "antd";
 import { FaLinkedin } from "react-icons/fa";
-
+import { Route, Routes } from "react-router-dom";
 import {
-  Navbar,
-  HomePage,
-  CryptoDetails,
-  Cryptocurrencies,
-  News,
-  Login,
-} from "./component/index"; // Import your components
-import { auth } from "./firebase/firebase"; // Import Firebase authentication
-import { Link } from "react-router-dom";
+    CryptoDetails,
+    Cryptocurrencies,
+    HomePage,
+    Loader,
+    Login,
+    Navbar,
+    News,
+    Signup,
+} from "./component/index";
+
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { login, logout, selectUser } from "./app/userSlice"; // Import Redux actions and selectors
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
+import { login, logout, selectUser } from "./app/userSlice";
+import { auth } from "./firebase/firebase";
 
 const App = () => {
-  const { Title } = Typography;
-  const user = useSelector(selectUser); // Get user data from Redux store
-  const dispatch = useDispatch(); // Initialize Redux dispatch function
+    const { Title } = Typography;
+    const user = useSelector(selectUser);
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(true); // loading state
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
-      // Listen for changes in the user's authentication state
-      if (userAuth) {
-        // If user is authenticated
-        dispatch(
-          login({
-            uid: userAuth.uid,
-            email: userAuth.email,
-          })
-        ); // Dispatch a login action to store user data in Redux
-      } else {
-        // If user is not authenticated
-        dispatch(logout()); // Dispatch a logout action to clear user data in Redux
-      }
-    });
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+            if (userAuth) {
+                dispatch(login({ uid: userAuth.uid, email: userAuth.email }));
+            } else {
+                dispatch(logout());
+            }
+            setLoading(false); // auth check complete
+        });
+        return () => unsubscribe();
+    }, [dispatch]);
 
-    return () => {
-      unsubscribe(); // Unsubscribe from the authentication state change listener
-    };
-  }, [dispatch]); // Only run this effect when the `dispatch` function changes
+    if (loading)
+        return (
+            <div className="loader">
+                <Loader />
+            </div>
+        );
 
-  return (
-    <>
-      {!user ? (
-        // If user is not authenticated, show the login component
-        <Login />
-      ) : (
-        // If user is authenticated, show the main app content
-        <div className="app">
-          <div className="navbar">
-            <Navbar />
-          </div>
-
-          <div className="main">
-            <Layout>
-              <div className="routes">
+    return (
+        <>
+            {!user ? (
                 <Routes>
-                  <Route index path="/" element={<HomePage />} />
-                  <Route path="/crypto/:coinId" element={<CryptoDetails />} />
-                  <Route
-                    exact
-                    path="/cryptocurrencies"
-                    element={<Cryptocurrencies />}
-                  />
-                  <Route exact path="/news" element={<News />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="*" element={<Login />} />{" "}
+                    {/* redirect unknown paths */}
                 </Routes>
-              </div>
-            </Layout>
-            <footer className="footer">
-              <Title
-                level={2}
-                style={{
-                  color: "#fff",
-                  textAlign: "center",
-                  textTransform: "capitalize",
-                }}
-              >
-                Crypto World All rights reserved
-              </Title>
-              <Space>
-                <Link to="/">Home</Link>
-                <Link to="/news">News</Link>
-                <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-                <Link
-                  to="https://linkedin.com/in/olumide-oluwuyi-66650b26a"
-                  target="_blank"
-                >
-                  <FaLinkedin />
-                </Link>
-              </Space>
-            </footer>
-          </div>
-        </div>
-      )}
-    </>
-  );
+            ) : (
+                <>
+                    <div className="app">
+                        <Navbar />
+                        <Layout className="main">
+                            <Routes>
+                                <Route path="/home" element={<HomePage />} />
+                                <Route
+                                    path="/crypto/:coinId"
+                                    element={<CryptoDetails />}
+                                />
+                                <Route
+                                    path="/cryptocurrencies"
+                                    element={<Cryptocurrencies />}
+                                />
+                                <Route path="/news" element={<News />} />
+                                <Route path="*" element={<HomePage />} />{" "}
+                                {/* fallback */}
+                            </Routes>
+                        </Layout>
+                    </div>
+
+                    <footer className="footer">
+                        <Title
+                            level={2}
+                            style={{ color: "#fff", textAlign: "center" }}
+                        >
+                            Crypto World All rights reserved
+                        </Title>
+                        <Space>
+                            <a href="/home">Home</a>
+                            <a href="/news">News</a>
+                            <a href="/cryptocurrencies">Cryptocurrencies</a>
+                            <a
+                                href="https://linkedin.com/in/olumide-oluwuyi-66650b26a"
+                                target="_blank"
+                            >
+                                <FaLinkedin />
+                            </a>
+                        </Space>
+                    </footer>
+                </>
+            )}
+        </>
+    );
 };
 
 export default App;
